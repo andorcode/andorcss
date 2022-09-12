919bee699411b2f4540c381a35099f176f67f5a2
Andrey Orlov <blogmycomp@gmail.com>
2022-09-12 20:45:02 +0300

    fix config

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
-export default {
+module.exports = {
 	'layout_path': './layout.css',
 	'content': ['./'],
 	'content_ext': ['html', 'php'],
 	'media': {
-		//'2xl': '1536px',
 		'xl': '1280px',
 		'lg': '1024px',
 		'md': '768px',
 		'sm': '512px',
 	},
-	//'extend': {},
 	'template': {
 		'colors': {
 			'black': '#000',
